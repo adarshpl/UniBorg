4684ebe5934b9d0b100b41b19d76dceab7f65a48
adarshpl <49791223+adarshpl@users.noreply.github.com>
2019-07-22 20:21:18 +0530

    Sticker.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -46,8 +46,8 @@ async def _(event):
         return
     me = borg.me
     userid = event.from_id
-    packname = f"@adarshpl s @UniBorg Pack"
-    packshortname = f"Uni_Borg_@adarshpl "  # format: Uni_Borg_userid
+    packname = f"@adarshpl @UniBorg Pack"
+    packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
 
     await event.edit("Processing this sticker. Please Wait!")
 
